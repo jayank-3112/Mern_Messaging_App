a2050a8295e9712d1c6598dfeea4eb20cc6f67d6
Jayank Mahaur <jayankmahaur@gmail.com>
2024-09-08 21:16:13 +0530

    Fixed apis

--- server/app.js
+++ server/app.js
@@ -72,7 +72,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('*',(req,res)=>{
   res.sendFile(path.join(__dirname,'public','index.html'));
 });
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("Hello World");
 });
 
