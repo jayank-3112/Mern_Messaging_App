d5fccc73b23c2c5939fa312d3468eb601830d362
Jayank Mahaur <jayankmahaur@gmail.com>
2024-09-08 19:45:47 +0530

    Added static files into the backend

--- server/app.js
+++ server/app.js
@@ -62,6 +62,14 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/chat", chatRoute);
 app.use("/api/v1/admin", adminRoute);
 
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'public','index.html'));
+});
+
+
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
